a6cbf31c3e0578ff36620630e44fc6652f5605bf
mohammad sawas <mohammad.sawas2016@gmail.com>
2018-02-14 13:02:59 +0000

    this is how we do it

--- comupter-speech/voice.py
+++
@@ -1,9 +0,0 @@
-from gtts import gTTS
-import os
-import sys
-
-name = sys.argv[1:4]
-name = " ".join(name)
-voice = gTTS(text='hello'+name, lang='en')
-voice.save("dude.mp3")
-os.system("mpg321 dude.mp3")
